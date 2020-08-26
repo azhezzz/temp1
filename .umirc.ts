b89abb8b51fbd0ab1584e607923df9be4b1c5533
azhezzz <azhezzzz@gmail.com>
2020-08-26 20:21:01 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  dva: {
+    immer: true,
+    hmr: false,
+  },
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
+});
